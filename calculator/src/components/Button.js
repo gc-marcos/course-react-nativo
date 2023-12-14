75aa7fe0ed88f6f9bd1afd6fcab4265721370477
Marcos Carvalho <gcmarcoss@gmail.com>
2023-12-14 15:28:01 -0300

    Calculadora #2
    Parte final da calculadora.

--- calculator/src/components/Button.js
+++ calculator/src/components/Button.js
@@ -6,17 +6,32 @@ const styles = StyleSheet.create({
     fontSize: 40,
     height: Dimensions.get('window').width / 4,
     width: Dimensions.get('window').width / 4,
-    // backgroundColor: '#F0F0F0',
+    padding: 20,
+    backgroundColor: '#000',
     textAlign: 'center',
     borderWidth: 1,
     borderColor: '#888',
   },
+  operationsButton: {
+    color: '#fff',
+    backgroundColor: '#fa8231',
+  },
+  buttonDouble: {
+    width: (Dimensions.get('window').width / 4) * 2,
+  },
+  buttonTriple: {
+    width: (Dimensions.get('window').width / 4) * 3,
+  },
 });
 
 export default props => {
+  const stylesButton = [styles.button];
+  if (props.double) stylesButton.push(styles.buttonDouble);
+  if (props.triple) stylesButton.push(styles.buttonTriple);
+  if (props.operation) stylesButton.push(styles.operationsButton);
   return (
-    <TouchableHighlight onPress={props.onClick}>
-      <Text style={styles.button}>{props.label}</Text>
+    <TouchableHighlight onPress={() => props.onClick(props.label)}>
+      <Text style={stylesButton}>{props.label}</Text>
     </TouchableHighlight>
   );
 };
